0304ad37c99f50c17dcdbae62ed1276f4781cc65
Xavi1594 <xavi9415@hotmail.com>
2022-11-10 10:44:50 +0100

    hago primera funcion

---
+++ app.js
@@ -0,0 +1,22 @@
+const harina = "harina integral de centeno" ;
+const agua ="agua" ;
+const aguatemplada = "agua templada";
+const aceite = "aceite" ;
+const sal = "sal" ;
+const levadura = "levadura";
+const azucar = "azucar" ;
+const salsa = "salsa" ;
+const queso = "queso" ;
+const peperoni = "peperoni" ;
+const aceitunas = "aceitunas sin hueso" ;
+const champinones = "champiñones" ;
+let harinaPeso = "400grs" ;
+let aguaPeso = "100 grs" ;
+let aceitePeso = "50 grs" ;
+let levaduraPeso = "16 grs";
+let azucarPeso = "1 cucharadita de café" ;
+
+function prepararLevadura(){
+    console.log("mezclar el " + aguatemplada + "el" + azucar + "y la" + levadura + "." );
+}
+prepararLevadura();
